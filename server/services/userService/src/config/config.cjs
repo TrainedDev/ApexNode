74fe577ac2261251907b2c9ac16ccbc4db5f50c5
TrainedDev <yogesh1999developer@gmail.com>
2026-08-11 15:45:00 +0530

    update userSerivce config by adding production

--- server/services/userService/src/config/config.cjs
+++ server/services/userService/src/config/config.cjs
@@ -6,5 +6,9 @@ module.exports = {
   "development": {
     use_env_variable: "DB_URL",
     dialect: "postgres",
+  },
+  "production": {
+    use_env_variable: "DB_URL",
+    dialect: "postgres",
   }
 }
